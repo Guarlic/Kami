7f6bb00e7917079ed8c15c504a851e1546c38e36
Hyunu Jung <hyunujung0419@gmail.com>
2022-03-18 14:47:00 +0900

    Guarlic - edit commands/user.js, bboom.js, hooha.js

---
+++ commands/bboom.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "뿜",
+  description: "뿜빠리",
+  execute(msg) {
+    return msg.reply('뿜빠리빰빠바 빠리빠리빰뿜!!');
+  }
+}
--- commands/user.js
+++ commands/user.js
@@ -7,8 +7,9 @@ module.exports = {
           return [msg.reply(`앗 구알릭님!! 닉네임이 ${msg.member.displayName}(이)라서 못알아봤잖아요!`), msg.react('❤️')]
         else
           return [msg.reply('앗 구알릭님!!'), msg.react('❤️')]
-    } else {
+    } else if (msg.author.username == "Guarlic3432" || msg.member.displayName == "Guarlic3432")
+      return [msg.reply('앗 구알릭님!!.. 이 아니네?'), msg.react('⁉️')]
+    else
       return msg.reply(`당신은 ${msg.author.username}! 맞죠?`);
-    }
   }
 }
