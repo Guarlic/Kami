8fc40289d90e813db162f13e3d936af04d063f98
Hyunu Jung <hyunujung0419@gmail.com>
2022-03-18 21:51:17 +0900

    Guarlic - commands/kkrpp.js, ujjultv.js, edit - commands.json - ㄹㅇㅋㅋ

---
+++ commands/ujjultv.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "어쩔티비",
+  description: "저쩔티비~",
+  execute(client, msg) {
+    if (msg.author.discriminator == 4857)
+      return [msg.reply('응~ 저쩔티ㅂ.. 아앗! 주인님 죄송해요!!'), msg.react('😅')]
+    else
+      return msg.reply('응~ 저쩔티뷔~');
+  }
+}
