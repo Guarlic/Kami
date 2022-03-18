b9acd505266897b187da7ea17737aa5be3203186
Hyunu Jung <hyunujung0419@gmail.com>
2022-03-18 20:45:22 +0900

    Guarlic - edit commands/ping.js - 입니다

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
   name: "핑",
   discription: "핑을 알려줌",
   execute(client, msg) {
-    msg.reply(`🏓 퐁! 현재 핑은 ${Math.round(client.ws.ping)}ms`);
+    msg.reply(`🏓 퐁! 현재 핑은 ${Math.round(client.ws.ping)}ms 입니다!`);
   }
 }
