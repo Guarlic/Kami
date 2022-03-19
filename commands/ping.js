e56e9eed0b82ba669b52fe1868b78899f5e5d417
Hyunu Jung <hyunujung0419@gmail.com>
2022-03-19 11:44:38 +0900

    Guarlic - edit commands/ping.js - 로딩

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,7 @@ module.exports = {
   name: "핑",
   discription: "핑을 알려줌",
   execute(client, msg) {
+    msg.reply('로딩중..').then(msg=>msg.delete({timeout:"1000"}));
     msg.reply(`🏓 퐁! 현재 핑은 ${Math.round(client.ws.ping)}ms 입니다!`);
   }
 }
