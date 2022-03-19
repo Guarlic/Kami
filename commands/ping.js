9bb6e98d0b87a1061b13c940ab2354906799a3d5
Hyunu Jung <hyunujung0419@gmail.com>
2022-03-19 13:26:36 +0900

    Guarlic - edit commands/ping.js - rm Math.round

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,6 @@ module.exports = {
   discription: "핑을 알려줌",
   execute(client, msg) {
     msg.reply('로딩중..').then(msg=>msg.delete({timeout:"1000"}));
-    msg.reply(`🏓 퐁! 현재 핑은 ${Math.round(client.ws.ping)}ms 입니다!`);
+    msg.reply(`🏓 퐁! 현재 핑은 ${client.ws.ping}ms 입니다!`);
   }
 }
