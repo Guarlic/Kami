8cb230ace664eb1436a371d398192e1cc31d70f5
Hyunu Jung <hyunujung0419@gmail.com>
2022-03-21 20:51:34 +0900

    Guarlic - edit commands/user.js - indent

--- commands/user.js
+++ commands/user.js
@@ -3,10 +3,10 @@ module.exports = {
     description: "유저 이름을 알려줌",
     execute(client, msg) {
         if (msg.author.discriminator == 4857) {
-        if (msg.member.displayName != "Guarlic3432")
-            return [msg.reply(`앗 구알릭님!! 닉네임이 ${msg.member.displayName}(이)라서 못알아봤잖아요!`), msg.react('❤️')]
-        else
-            return [msg.reply('앗 구알릭님!!'), msg.react('❤️')]
+            if (msg.member.displayName != "Guarlic3432")
+                return [msg.reply(`앗 구알릭님!! 닉네임이 ${msg.member.displayName}(이)라서 못알아봤잖아요!`), msg.react('❤️')]
+            else
+                return [msg.reply('앗 구알릭님!!'), msg.react('❤️')]
         } 
         else if (msg.author.username == "Guarlic3432" || msg.member.displayName == "Guarlic3432")
             return [msg.reply('앗 구알릭님!!.. 이 아니네?'), msg.react('⁉️')]
