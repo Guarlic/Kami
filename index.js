044545010942706dfb20eeac8c3b1f112e20132c
Hyunu Jung <hyunujung0419@gmail.com>
2022-03-23 19:08:31 +0900

    Guarlic - edit commands.json - ㄴㅇㄱ, edit index.js - 없는 명령어 - 띄어쓰기 인식

--- index.js
+++ index.js
@@ -55,11 +55,12 @@ client.on("messageCreate", async msg => {
         }
     }
 
-    if (msg.content.slice(default_prefix.length + 1) == " " || msg.content.slice(default_prefix2.length) == " ") {
-        if (msg.content.startsWith(default_prefix2))
+    if (msg.content.startsWith(default_prefix) && msg.content.slice(default_prefix) != ' ') {
+        if (msg.content.startsWith(default_prefix2)) {
             msg.reply(`\`${msg.content.slice(default_prefix2.length)}\`..?`);
-        else if (msg.content.startsWith(default_prefix))
-            msg.reply(`\`${msg.content.slice(default_prefix.length + 1)}\`..?`);
+            return;
+        }
+        msg.reply(`\`${msg.content.slice(default_prefix.length + 1)}\`..?`);
     }
 });
 
